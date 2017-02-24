ce6cda341cd6c872985116f3d6407407a949fc2f
jonas <jonas8@me.com>
2017-02-24 10:56:17 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,18 @@
+# lodash-tpl-loader
+
+A simple [lodash template](https://lodash.com/docs/#template) loader for [webpack](http://webpack.github.io/). 
+
+## Install
+
+`npm install --save-dev lodash-tpl-loader`
+
+
+## Options
+
+See [lodash template options](https://lodash.com/docs/#template)
+
+## License
+
+MIT (http://www.opensource.org/licenses/mit-license.php)
+
+
---
+++ index.js
@@ -0,0 +1,9 @@
+var _ = require('lodash'),
+  loaderUtils = require('loader-utils')
+
+module.exports = function(source) {
+  this.cacheable && this.cacheable()
+  var options = this.query instanceof Object ? this.query : loaderUtils.parseQuery(this.query)
+  var compiled = _.compile(source, options)
+  return 'var _ = require(\'lodash\');module.exports = ' + compiled
+}
