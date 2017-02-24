f9df5129353b7d90db6d45be5d228b4096c7005a
jonas <jonas8@me.com>
2017-02-24 11:00:58 +0800

    bug fix

--- index.js
+++ index.js
@@ -4,6 +4,6 @@ var _ = require('lodash'),
 module.exports = function(source) {
   this.cacheable && this.cacheable()
   var options = this.query instanceof Object ? this.query : loaderUtils.parseQuery(this.query)
-  var compiled = _.compile(source, options)
+  var compiled = _.template(source, options)
   return 'var _ = require(\'lodash\');module.exports = ' + compiled
 }
